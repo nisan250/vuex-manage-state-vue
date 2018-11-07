b7aa34bce83b94224c667e477ac2d26388ee007b
nisan250 <nisan250@gmail.com>
2018-11-07 20:19:39 +0000

    adding project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Vuex State Management</h1>
+    <div class="products">
+      <h2>Products</h2>
+        <Product
+          v-for="product in products"
+          v-bind:key="product.id"
+          v-bind:id="product.id"
+          v-bind:name="product.name"
+          v-bind:price="product.price"
+        />
+    </div>
+    <Cart />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import store from "@/store.js";
+import Cart from "./components/Cart.vue";
+import Product from "./components/Product.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  store,
   components: {
-    HelloWorld
+    Cart,
+    Product
+  },
+  data() {
+    return store.state;
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 0 auto;
   margin-top: 60px;
+  max-width: 800px;
+}
+
+.products {
+  margin-right: 45px;
+  border-right: 5px solid black;
+  width: 400px;
+  float: left;
+}
+
+.component-product {
+  margin-bottom: 5px;
+}
+
+.cart {
+  float: left;
 }
 </style>
